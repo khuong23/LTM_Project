debfee19cd150061b549443334a6471b5929ce94
khuong <baokhuongtran1234@gmail.com>
2025-11-19 23:38:41 +0700

    Add setting properties and modified gitignore

--- src/main/java/com/ltmproject/utils/DBConnection.java
+++ src/main/java/com/ltmproject/utils/DBConnection.java
@@ -1,13 +1,32 @@
 package com.ltmproject.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/LTM_Project";
-    private static final String USER = "root";
-    private static final String PASSWORD = "baokhuong2332";
+    private static final String PROPERTIES_FILE = "settings.properties";
+
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try (InputStream input = new FileInputStream(PROPERTIES_FILE)) {
+            Properties prop = new Properties();
+            prop.load(input);
+            URL = prop.getProperty("db.url");
+            USER = prop.getProperty("db.username");
+            PASSWORD = prop.getProperty("db.password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Không thể đọc file cấu hình");
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         try {
