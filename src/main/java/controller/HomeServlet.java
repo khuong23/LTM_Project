10d812efebcd0360f0b62d979ec7725e8beddd1a
LOngkt1023 <longkt0412@gmail.com>
2025-11-20 09:14:55 +0700

    hoan thien problemBO va Servelet

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -1,25 +1,24 @@
 package controller;
 
+import model.BO.ProblemsBO;
+import model.Bean.Problems;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import model.Bean.Problems;
-import model.DAO.ProblemsDAO;
-
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name = "HomeServlet", value = "/Home")
 public class HomeServlet extends HttpServlet {
+    private ProblemsBO problemsBO = new ProblemsBO();
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProblemsDAO problemsDAO = new ProblemsDAO();
-        try {
-            List<Problems> list = problemsDAO.getAllProblems();
-            request.setAttribute("problemList", list);
-            request.getRequestDispatcher("user_page.jsp").forward(request, response);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        List<Problems> list = problemsBO.getAllProblems();
+
+        request.setAttribute("problemList", list);
+
+        request.getRequestDispatcher("user_page.jsp").forward(request, response);
     }
 }
---
+++ src/main/java/model/BO/ProblemsBO.java
@@ -0,0 +1,20 @@
+package model.BO;
+
+import model.Bean.Problems;
+import model.DAO.ProblemsDAO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemsBO {
+    private ProblemsDAO problemsDAO = new ProblemsDAO();
+
+    public List<Problems> getAllProblems() {
+        try {
+            return problemsDAO.getAllProblems();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
