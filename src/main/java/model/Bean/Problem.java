87326676b3b8ce022af08764397501e9fd3434a5
khuong23 <baokhuongtran1234@gmail.com>
2025-11-20 08:43:04 +0700

    Fix Bean properties name

--- src/main/java/model/Bean/Problem.java
+++
@@ -1,8 +0,0 @@
-package model.Bean;
-
-public class Problem {
-    private int id;
-    private String title;
-    private String description;
-
-}
---
+++ src/main/java/model/Bean/Problems.java
@@ -0,0 +1,22 @@
+package model.Bean;
+
+public class Problems {
+    private int problem_id;
+    private String description;
+    Problems(int problem_id, String description) {
+        this.problem_id = problem_id;
+        this.description = description;
+    }
+    public int getProblem_id() {
+        return problem_id;
+    }
+    public void setProblem_id(int problem_id) {
+        this.problem_id = problem_id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
