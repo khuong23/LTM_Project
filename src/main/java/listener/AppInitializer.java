db4877e92ca765359de826a3652806e6c49ad80a
khuong <baokhuongtran1234@gmail.com>
2025-11-22 12:44:08 +0700

    Add logging JudgeManager when start or stop in AppInitializer

--- src/main/java/listener/AppInitializer.java
+++ src/main/java/listener/AppInitializer.java
@@ -10,11 +10,13 @@ public class AppInitializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("[AppInitializer] Starting JudgeManager...");
         JudgeManager.start();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("[AppInitializer] Stopping JudgeManager...");
         JudgeManager.stop();
     }
 }
